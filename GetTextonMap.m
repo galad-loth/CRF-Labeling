02c814509fd663e704d5ff56e60d94051512ba11
galad-loth <jlfeng911@gmail.com>
2016-12-06 21:06:06 +0800

    Add GetTextonMap

---
+++ GetTextonMap.m
@@ -0,0 +1,19 @@
+function textonMap=GetTextonMap(modeltexton,imgPath,imgList)
+numImg=length(imgList);
+textonMap=cell(1,numImg);
+for kk=1:numImg
+    disp(['Processing the ', num2str(kk),'-th image.']);
+    imgFile=imgList{kk};
+    img=imread([imgPath,imgFile]);
+    img=double(img);
+    [nr,nc,nd]=size(img);
+    featTemp=TextonFiltering(img,modeltexton.filters);
+    featTemp=bsxfun(@minus,featTemp,modeltexton.normMean);
+    featTemp=bsxfun(@times,featTemp,1./(modeltexton.normVar));
+    featTemp=featTemp*modeltexton.matWhitten;
+    matDist=pdist2(featTemp,modeltexton.textonMean);
+    [~,textonLabel]=min(matDist,[],2);
+    textonLabel=reshape(textonLabel,[nr nc]);
+    textonMap{kk}=textonLabel;    
+end
+
