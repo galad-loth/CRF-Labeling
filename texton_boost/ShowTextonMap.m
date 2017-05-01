107153cb758e15babaa22dfd58fa508bbaa8d3fb
Galad_Loth <jlfeng911@gmail.com>
2017-05-01 11:50:44 +0800

    Update ShowTextonMap.m

--- texton_boost/ShowTextonMap.m
+++ texton_boost/ShowTextonMap.m
@@ -1,12 +1,14 @@
 function ShowTextonMap()
 dataInfo=SetMSRC21DataInfo();
-load textonMapTest
+load textonMapTrain
+load('data\modelTexton.mat')
 
 numImg=length(dataInfo.trainImgList);
 figure(1);set(gcf,'position',[200 400 1300 400])
 for kk=1:numImg
-    img=imread([dataInfo.imgPath,dataInfo.testImgList{kk}]);
+    img=imread([dataInfo.imgPath,dataInfo.trainImgList{kk}]);
     subplot(1,2,1);imagesc(img)
-    subplot(1,2,2);imagesc(textonMapTest{kk},[0 128]);
+    classmap=GetClassMap(textonMapTrain{kk}-1,modelTexton.colormap);
+    subplot(1,2,2);imagesc(classmap);
     pause    
 end
